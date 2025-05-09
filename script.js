9ad422a7c5b2111bc4729a685c47aec4a20f241c
hearu28 <hearu28@naver.com>
2025-05-09 17:07:54 +0900

    add readme

--- script.js
+++ script.js
@@ -1,41 +1,63 @@
 const pressedKeys = new Set();
+const activeSources = new Map(); // 키에 대응하는 재생 노드 저장
+const context = new (window.AudioContext || window.webkitAudioContext)();
+let sharedBuffer = null;
+
+const playbackRates = {
+  1: 0.5946,
+  2: 0.6674,
+  3: 0.7491,
+  4: 0.8409,
+  5: 0.9441,
+  6: 1.0,
+  7: 1.0595,
+  8: 1.1225,
+  9: 1.1892,
+  0: 1.2599,
+};
 
 document.addEventListener("keydown", (e) => {
   const key = e.key;
-  if (!pressedKeys.has(key)) {
+  if (!pressedKeys.has(key) && isPlayableKey(key)) {
     pressedKeys.add(key);
-    if (isPlayableKey(key)) {
-      playSound(key);
-      animateOtamatone();
-    }
+    playSound(key);
+    animateOtamatone();
   }
 });
 
 document.addEventListener("keyup", (e) => {
-  pressedKeys.delete(e.key);
+  const key = e.key;
+  pressedKeys.delete(key);
+
+  if (activeSources.has(key)) {
+    const source = activeSources.get(key);
+    source.stop();
+    activeSources.delete(key);
+  }
 });
 
 function isPlayableKey(key) {
-  return ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"].includes(key);
+  return Object.keys(playbackRates).includes(key);
+}
+
+function loadBuffer() {
+  return fetch("sounds/base_note.mp3")
+    .then((res) => res.arrayBuffer())
+    .then((data) => context.decodeAudioData(data))
+    .then((buffer) => {
+      sharedBuffer = buffer;
+    });
 }
 
 function playSound(key) {
-  const context = new (window.AudioContext || window.webkitAudioContext)();
-  const request = new XMLHttpRequest();
-  request.open("GET", "sounds/base_note.mp3", true);
-  request.responseType = "arraybuffer";
+  if (!sharedBuffer) return;
 
-  request.onload = function () {
-    context.decodeAudioData(request.response, function (buffer) {
-      const source = context.createBufferSource();
-      source.buffer = buffer;
-      source.playbackRate.value = playbackRates[key];
-      source.connect(context.destination);
-      source.start(0);
-    });
-  };
-
-  request.send();
+  const source = context.createBufferSource();
+  source.buffer = sharedBuffer;
+  source.playbackRate.value = playbackRates[key];
+  source.connect(context.destination);
+  source.start();
+  activeSources.set(key, source);
 }
 
 function animateOtamatone() {
@@ -44,15 +66,5 @@ function animateOtamatone() {
   setTimeout(() => otamatone.classList.remove("playing"), 150);
 }
 
-const playbackRates = {
-  "1": 0.5946,
-  "2": 0.6674,
-  "3": 0.7491,
-  "4": 0.8409,
-  "5": 0.9441,
-  "6": 1.0,
-  "7": 1.0595,
-  "8": 1.1225,
-  "9": 1.1892,
-  "0": 1.2599
-};
+// 첫 로딩 시 오디오 버퍼 미리 로드
+loadBuffer();
